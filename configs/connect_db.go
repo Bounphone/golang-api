79f10e1ac2d6261a848d213a4a09d739f842797c
Bounphone KOSADA <olaybounphonekosada@gmail.com>
2022-10-16 20:16:52 +0700

    Update connect_db.go

--- configs/connect_db.go
+++ configs/connect_db.go
@@ -30,12 +30,12 @@ func ConnectDB() *mongo.Client {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
 	defer client.Disconnect(ctx)
 	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
 
 	return client
