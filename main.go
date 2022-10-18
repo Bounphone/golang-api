3c6c58138e15656b520dd9056f2d013024cd7b4d
Bounphone KOSADA <olaybounphonekosada@gmail.com>
2022-10-18 22:51:00 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,28 +1,46 @@
+// package main
+
+// import (
+// 	"crypto/tls"
+// 	"golang_rest_api/configs"
+// 	"golang_rest_api/routes"
+// 	"net/http"
+
+// 	// "log"
+// 	// "os"
+
+// 	"github.com/gin-gonic/gin"
+// 	// "github.com/joho/godotenv"
+// )
+
+//	func main() {
+//		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+//		// err := godotenv.Load("fly.toml")
+//		// if err != nil {
+//		// 	log.Fatalf("Error loading .env file")
+//		// }
+//		configs.ConnectDB()
+//		r := gin.Default()
+//		routes.UserProfileRoutes(r)
+//		r.Run()
+//		// port := os.Getenv("PORT")
+//		// r.Run(":" + port)
+//	}
 package main
 
 import (
-	"crypto/tls"
-	"golang_rest_api/configs"
-	"golang_rest_api/routes"
+	"io"
+	"log"
 	"net/http"
-
-	// "log"
-	// "os"
-
-	"github.com/gin-gonic/gin"
-	// "github.com/joho/godotenv"
+	"os"
 )
 
 func main() {
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	// err := godotenv.Load("fly.toml")
-	// if err != nil {
-	// 	log.Fatalf("Error loading .env file")
-	// }
-	configs.ConnectDB()
-	r := gin.Default()
-	routes.UserProfileRoutes(r)
-	r.Run()
-	// port := os.Getenv("PORT")
-	// r.Run(":" + port)
+	port := os.Getenv("PORT")
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+	http.HandleFunc("/", helloHandler)
+	log.Println("Listing for" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
